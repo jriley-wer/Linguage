f5b6d5baad90d135ba393885419fd0d48bb04120
Sydney Werbach <sydwer@gmail.com>
2019-12-03 13:47:08 -0700

    updated comparison controller to include: the languages within the comaprison

--- backend/database/app/controllers/comparisons_controller.rb
+++ backend/database/app/controllers/comparisons_controller.rb
@@ -1,11 +1,11 @@
 class ComparisonsController < ApplicationController
     def index
         comparisons = Comparison.all 
-        render json: comparisons
+        render json: comparisons, include: [:native_language, :target_language]
     end
 
     def show
         comparison = Comparison.find(params[:id])
-        render json: comparison
+        render json: comparison include: [:native_language, :target_language]
     end
 end
